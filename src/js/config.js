0b29fe7cc05420604d0bc4d62b81abbba9c3a0b2
Rahul <rahulchandlawala@gmail.com>
2022-04-26 09:03:09 +0530

    first commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '9ef6f402-6b24-40db-beb7-8f7a43c5688e';
+export const MODAL_CLOSE_SEC = 2.5;
